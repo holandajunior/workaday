244c9c725af1b8b96654241be9d8bc76b3dbbfa9
Holanda Junior <joaoholandajr@gmail.com>
2017-04-30 22:33:28 -0300

    Cleaning imports

--- query-side/src/main/java/br/holandajunior/workaday/Application.java
+++ query-side/src/main/java/br/holandajunior/workaday/Application.java
@@ -1,16 +1,11 @@
 package br.holandajunior.workaday;
 
-import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.broker.BrokerService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
-import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
-import org.springframework.jms.config.JmsListenerContainerFactory;
 
-import javax.jms.ConnectionFactory;
 
 /**
  * Created by holandajunior on 29/04/17.
