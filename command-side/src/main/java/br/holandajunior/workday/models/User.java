31e589c0f2c6185129420b00ee683ac92b8a84b6
Holanda Junior <joaoholandajr@gmail.com>
2017-05-02 10:24:00 -0300

    Mapping models properly with jpa; Doing bidirectional oneToMany relationship between user and point

--- command-side/src/main/java/br/holandajunior/workday/models/User.java
+++ command-side/src/main/java/br/holandajunior/workday/models/User.java
@@ -1,24 +1,26 @@
 package br.holandajunior.workday.models;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.List;
 
 /**
  * Created by holandajunior on 29/04/17.
  */
 
 @Entity
+@Table(name = "workaday_user") // If not set up, 'user' in postgres is a keyword, then table will not be created properly
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     private String name;
     private String email;
     private String password;
+
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Point> points;
 
     public long getId() {
         return id;
@@ -47,6 +49,21 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public List<Point> getPoints() {
+        return points;
+    }
+
+    public void setPoints(List<Point> points) {
+        this.points = points;
+    }
+
+    public void addPoint( Point point ) {
+
+        this.points.add( point );
+        point.setUser( this );
+    }
+
 
     @Override
     public boolean equals(Object o) {
