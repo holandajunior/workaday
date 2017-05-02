aeea3767d497c8abdd4967e28a01317b068c5348
Holanda Junior <joaoholandajr@gmail.com>
2017-05-02 10:20:49 -0300

    Updating events destination; Add listener to point updated event

--- query-side/src/main/java/br/holandajunior/workaday/projections/UserProjections.java
+++ query-side/src/main/java/br/holandajunior/workaday/projections/UserProjections.java
@@ -1,6 +1,7 @@
 package br.holandajunior.workaday.projections;
 
 import br.holandajunior.workaday.events.point.PointCreated;
+import br.holandajunior.workaday.events.point.PointUpdated;
 import br.holandajunior.workaday.events.user.UserCreated;
 import br.holandajunior.workaday.model.Point;
 import br.holandajunior.workaday.model.User;
@@ -8,6 +9,8 @@ import br.holandajunior.workaday.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
 
 /**
  * Created by holandajunior on 30/04/17.
@@ -19,22 +22,25 @@ public class UserProjections {
     @Autowired
     private UserRepository userRepo;
 
-    @JmsListener( destination = UserCreated.DESTINATION)
+    @JmsListener(destination = UserCreated.DESTINATION)
     public void onUserCreated( UserCreated userCreated ) {
 
         User user = new User();
+        user.setUserId( userCreated.getUserId() );
         user.setName( userCreated.getName() );
         user.setEmail( userCreated.getEmail() );
+        user.setPoints(new ArrayList<Point>());
 
         userRepo.save( user );
     }
 
-    @JmsListener( destination = PointCreated.DESTINATION)
+    @JmsListener(destination = PointCreated.DESTINATION)
     public void onPointCreated(PointCreated pointCreated) {
 
-        User user = userRepo.findOne( pointCreated.getUserId() );
+        User user = userRepo.findByUserId( pointCreated.getUserId() );
 
         Point point = new Point();
+        point.setPointId( pointCreated.getPointId() );
         point.setEntry( pointCreated.getEntry() );
         point.setExit( pointCreated.getExit() );
 
@@ -42,5 +48,13 @@ public class UserProjections {
 
         userRepo.save( user );
     }
+
+    @JmsListener(destination = PointUpdated.DESTINATION)
+    public void onPointUpdated(PointUpdated pointUpdated) {
+
+        userRepo.updatePoint( pointUpdated );
+    }
+
+
 
 }
