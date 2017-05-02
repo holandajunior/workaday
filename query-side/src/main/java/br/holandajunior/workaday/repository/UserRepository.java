99e5f8c583fb0970a3539b44a112d126eb5b3beb
Holanda Junior <joaoholandajr@gmail.com>
2017-05-02 10:21:09 -0300

    Filtering fields to be returned from query

--- query-side/src/main/java/br/holandajunior/workaday/repository/UserRepository.java
+++ query-side/src/main/java/br/holandajunior/workaday/repository/UserRepository.java
@@ -2,11 +2,17 @@ package br.holandajunior.workaday.repository;
 
 import br.holandajunior.workaday.model.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
+
+import java.util.List;
 
 /**
  * Created by holandajunior on 01/05/17.
  */
 public interface UserRepository extends MongoRepository<User, String >, UserRepositoryCustom {
+
+    @Query(value = "{}", fields = "{ 'name':1, 'email':1 }")
+    List<User> findAll();
 
     User findByUserId( long userId );
 }
