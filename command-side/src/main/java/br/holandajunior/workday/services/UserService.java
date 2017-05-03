7704ff9d8b38e33eee7ea8c2c9b95466b51e2acc
Holanda Junior <joaoholandajr@gmail.com>
2017-05-02 23:16:41 -0300

    When adding new user, encoding password

--- command-side/src/main/java/br/holandajunior/workday/services/UserService.java
+++ command-side/src/main/java/br/holandajunior/workday/services/UserService.java
@@ -2,11 +2,13 @@ package br.holandajunior.workday.services;
 
 import br.holandajunior.workaday.events.user.UserCreated;
 import br.holandajunior.workday.commands.CreateUser;
-import br.holandajunior.workday.models.User;
+import br.holandajunior.workday.models.repository.User;
 import br.holandajunior.workday.repositories.IUserRepository;
 import br.holandajunior.workday.services.api.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 /**
@@ -24,17 +26,20 @@ public class UserService implements IUserService {
 
     public void save(CreateUser createUserCommand) {
 
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String hashedPassword = passwordEncoder.encode( createUserCommand.getPassword() );
+
         // Persist new user
         User user = new User();
-        user.setName( createUserCommand.getName() );
+        user.setUsername( createUserCommand.getUsername() );
         user.setEmail( createUserCommand.getEmail() );
-        user.setPassword( createUserCommand.getPassword() );
+        user.setPassword( hashedPassword );
 
         User newUser = userRepository.saveAndFlush( user );
 
         // Send event
         UserCreated userCreated = new UserCreated( newUser.getId(),
-                createUserCommand.getName(),
+                createUserCommand.getUsername(),
                 createUserCommand.getEmail() );
 
         jmsTemplate.convertAndSend( UserCreated.DESTINATION, userCreated );
