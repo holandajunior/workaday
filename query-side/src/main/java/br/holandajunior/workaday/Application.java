2b48bf09a13de342e9aad08b6345dee4ddc410db
Holanda Junior <joaoholandajr@gmail.com>
2017-05-04 20:38:47 -0300

    Add cors filter into query-side

--- query-side/src/main/java/br/holandajunior/workaday/Application.java
+++ query-side/src/main/java/br/holandajunior/workaday/Application.java
@@ -5,6 +5,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 
 /**
@@ -25,6 +28,16 @@ public class Application {
         broker.setPersistent(false);
 
         return broker;
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("*");
+            }
+        };
     }
 
     public static void main(String[] args) {
