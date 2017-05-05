e903be8b9876ff74ce6a9633a376dc79d9540d98
Holanda Junior <joaoholandajr@gmail.com>
2017-05-04 22:43:18 -0300

    Reseting signup form when submitted succesfully

--- frontend/src/rootFormReducer.js
+++ frontend/src/rootFormReducer.js
@@ -2,9 +2,22 @@ import { reducer as formReducer } from 'redux-form';
 
 // Internal dependencies
 import { pointActionTypes } from './admin';
+import login from './login';
 
 const reducer = formReducer.plugin({
 	
+	signupForm: ( state, action ) => {
+
+		switch( action.type ) {
+
+			case login.actionTypes.SIGNUP_SUCCESS:
+				return undefined;
+			
+			default:
+				return state;		
+		}
+	},
+
 	point: ( state, action ) => {
 
 		switch( action.type ) {
@@ -16,6 +29,7 @@ const reducer = formReducer.plugin({
 				return state;		
 		}
 	}
+
 });
 
 export default reducer;
